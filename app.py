cbb40bf9cc5ec5811466e2042c80d31e2390a105
ShubhamSKKumar <shubhamsk23kumar@gmail.com>
2026-09-14 02:09:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.write("Upload an image of the sky, and the ResNet50 AI will predict the weath
 
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model('models/resnet50_weather_classifier.keras')
+    return tf.keras.models.load_model('models/02_resnet50_weather_classifier.keras')
 
 model = load_model()
 classes = ['Cloudy', 'Rain', 'Shine', 'Sunrise'] 
